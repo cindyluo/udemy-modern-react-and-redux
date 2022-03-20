76e2d416c775ccafe38f81e88c550d4fdd2b88eb
Cindy Luo <cindyluo@gogolook.com>
2022-03-21 02:01:16 +0800

    add ch12 widgets

---
+++ ch12_widgets/src/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end javascript framework',
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite JS library among engineers',
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating components',
+  },
+];
+
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red',
+  },
+  {
+    label: 'The Color Green',
+    value: 'green',
+  },
+  {
+    label: 'The shade of Blue',
+    value: 'blue',
+  },
+];
+
+const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
+
+  return (
+    <div>
+      <Accordion items={items} />
+      <Search />
+      <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
+      {showDropdown ? <Dropdown label="Select a Color" selected={selected} onSelectedChange={setSelected} options={options} /> : null}
+      <Translate />
+    </div>
+  );
+};
+
+export default App;
